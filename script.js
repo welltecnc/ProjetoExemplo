045206c9fedaf872f57fe81759ab40f2c35405a3
Wellington <welltecnic@hotmail.com>
2024-09-19 11:23:08 -0300

    criando tipos de variaveis

--- script.js
+++ script.js
@@ -21,8 +21,30 @@ console.log(nome1)
 const nome2="javascript";
 console.log(nome2)
 
+//TIPOS DE VARIAVEIS 
 
+//INTEIRO
+let idade = 18;
+console.log(idade)
 
+//FLUTUANTES OU COM CASAS DECIMAIS
+
+let valor= 6.40;
+console.log(valor)
+
+//STRING (TEXTOS) COM ASPAS DUPLAS
+let texto="Fiap";
+console.log(texto)
+
+//BOOLEANOS - VERDADEIRO OU FALSO
+
+let aluno=true
+console.log(aluno)
+
+//ARRAY- CONJUNTO DE INFORMAÇÕES
+let tecnologias =["JAVA","C#","HTML","CSS"];
+
+console.log(tecnologias)
 
 
 

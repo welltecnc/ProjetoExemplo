a9066d0f2498addedd69be5b346f60a64fdfbf49
Wellington <welltecnic@hotmail.com>
2024-09-19 11:07:49 -0300

    declarando os tipos de dados var, let ,const

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Ola Mundo-externo")
+// alert("Ola Mundo-externo")
+
+//DECLARANDO AS VARIAVEIS 
+
+var nome="fiap";
+console.log(nome)
+
+let nome1="cidade";
+console.log(nome1)
+
+const nome2="javascript";
+console.log(nome2)
